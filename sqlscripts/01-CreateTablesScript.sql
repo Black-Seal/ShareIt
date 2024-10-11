ce8d26367e0adfd7a7df445974a09fc58199f698
quayjunwei <quayjunwei@outlook.com>
2024-10-11 23:32:12 +0800

    updated sql script

--- sqlscripts/01-CreateTablesScript.sql
+++ sqlscripts/01-CreateTablesScript.sql
@@ -1,49 +1,49 @@
 -- Users table
-CREATE TABLE [dbo].[Users] (
-    [User_ID]   INT IDENTITY(1,1) NOT NULL,  -- Auto-incrementing User_ID
+CREATE TABLE [dbo].[users] (
+    [UserID]   INT IDENTITY(1,1) NOT NULL,  -- Auto-incrementing UserID
     [FirstName] VARCHAR (255) NOT NULL,
     [LastName]  VARCHAR (255) NOT NULL,
     [Email]     VARCHAR (255) NULL,
     [Address]   VARCHAR (255) NULL,
     [Contact]   VARCHAR (20)  NULL,
-    PRIMARY KEY CLUSTERED ([User_ID] ASC),   -- Primary Key on User_ID
+    PRIMARY KEY CLUSTERED ([UserID] ASC),    -- Primary Key on UserID
     UNIQUE NONCLUSTERED ([Email] ASC)        -- Unique constraint on Email
 );
 
 -- Items table
-CREATE TABLE [dbo].[Items] (
-    [Item_ID]     INT             NOT NULL,
-    [User_ID]     INT             NOT NULL,
+CREATE TABLE [dbo].[items] (
+    [ItemID]     INT             NOT NULL,
+    [UserID]     INT             NOT NULL,
     [Item_Name]   VARCHAR (255)   NOT NULL,
     [Description] VARCHAR (255)   NULL,
     [Price]       DECIMAL (10, 2) NOT NULL,
-    PRIMARY KEY CLUSTERED ([Item_ID] ASC),
-    FOREIGN KEY ([User_ID]) REFERENCES [dbo].[Users] ([User_ID])
+    PRIMARY KEY CLUSTERED ([ItemID] ASC),
+    FOREIGN KEY ([UserID]) REFERENCES [dbo].[users] ([UserID])
 );
 
 -- Listings table
-CREATE TABLE [dbo].[Listings] (
-    [Listing_ID]  INT  NOT NULL,
-    [Borrower_ID] INT  NOT NULL,
-    [Lender_ID]   INT  NOT NULL,
-    [Item_ID]     INT  NOT NULL,
-    [Start_Date]  DATE DEFAULT (getdate()) NOT NULL,
-    [End_Date]    DATE NOT NULL,
-    [Return_Flag] BIT  DEFAULT ((0)) NOT NULL,
-    PRIMARY KEY CLUSTERED ([Listing_ID] ASC),
-    FOREIGN KEY ([Borrower_ID]) REFERENCES [dbo].[Users] ([User_ID]),
-    FOREIGN KEY ([Item_ID]) REFERENCES [dbo].[Items] ([Item_ID]),
-    FOREIGN KEY ([Lender_ID]) REFERENCES [dbo].[Users] ([User_ID])
+CREATE TABLE [dbo].[listings] (
+    [ListingID]  INT  NOT NULL,
+    [BorrowerID] INT  NOT NULL,
+    [LenderID]   INT  NOT NULL,
+    [ItemID]     INT  NOT NULL,
+    [StartDate]  DATE DEFAULT (getdate()) NOT NULL,
+    [EndDate]    DATE NOT NULL,
+    [ReturnFlag] BIT  DEFAULT ((0)) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ListingID] ASC),
+    FOREIGN KEY ([BorrowerID]) REFERENCES [dbo].[Users] ([UserID]),
+    FOREIGN KEY ([ItemID]) REFERENCES [dbo].[Items] ([ItemID]),
+    FOREIGN KEY ([LenderID]) REFERENCES [dbo].[Users] ([UserID])
 );
 
 -- Fines table
-CREATE TABLE [dbo].[Fines] (
-    [Fine_ID]     INT             NOT NULL,
-    [Listing_ID]  INT             NOT NULL,
-    [Borrower_ID] INT             NOT NULL,
-    [Fine_Amount] DECIMAL (10, 2) NOT NULL,
-    [Fine_Date]   DATE            DEFAULT (getdate()) NOT NULL,
-    PRIMARY KEY CLUSTERED ([Fine_ID] ASC),
-    FOREIGN KEY ([Borrower_ID]) REFERENCES [dbo].[Users] ([User_ID]),
-    FOREIGN KEY ([Listing_ID]) REFERENCES [dbo].[Listings] ([Listing_ID])
+CREATE TABLE [dbo].[fines] (
+    [FineID]     INT             NOT NULL,
+    [ListingID]  INT             NOT NULL,
+    [BorrowerID] INT             NOT NULL,
+    [FineAmount] DECIMAL (10, 2) NOT NULL,
+    [FineDate]   DATE            DEFAULT (getdate()) NOT NULL,
+    PRIMARY KEY CLUSTERED ([FineID] ASC),
+    FOREIGN KEY ([BorrowerID]) REFERENCES [dbo].[users] ([UserID]),
+    FOREIGN KEY ([ListingID]) REFERENCES [dbo].[Listings] ([ListingID])
 );
